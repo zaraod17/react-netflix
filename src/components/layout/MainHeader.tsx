e33c3495d7e03b8e76886e9af8b3d15bbd82d741
mariusz <mariusz.waldoch98@gmail.com>
2022-10-21 21:22:48 +0200

    auth page

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -25,6 +27,10 @@ const pages = [
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const MainHeader: React.FC = () => {
+  const location = useLocation();
+
+  let isAuth = location.pathname === "/auth";
+
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
@@ -55,60 +61,100 @@ const MainHeader: React.FC = () => {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <AppBar position="sticky" color="primary">
-        <Toolbar disableGutters sx={{ marginInline: "2%" }}>
-          <Typography
-            variant="h5"
-            noWrap
-            component="p"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "red",
-              textDecoration: "none",
-            }}
-          >
-            Reactflix
-          </Typography>
-
-          <Box
-            sx={{
-              flexGrow: 1,
-              alignItems: "center",
-              display: { xs: "flex", md: "none" },
-            }}
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+      {!isAuth && (
+        <AppBar position="sticky" color="primary">
+          <Toolbar disableGutters sx={{ marginInline: "2%" }}>
+            <Typography
+              variant="h5"
+              noWrap
+              component="p"
               sx={{
-                display: { xs: "block", md: "none" },
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "red",
+                textDecoration: "none",
               }}
             >
+              Reactflix
+            </Typography>
+
+            <Box
+              sx={{
+                flexGrow: 1,
+                alignItems: "center",
+                display: { xs: "flex", md: "none" },
+              }}
+            >
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <NavLink
+                    key={page.title}
+                    style={({ isActive, isPending }) => {
+                      return {
+                        color: isActive ? "white" : "lightgrey",
+                        textDecoration: "none",
+                      };
+                    }}
+                    to={page.path}
+                  >
+                    <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page.title}</Typography>
+                    </MenuItem>
+                  </NavLink>
+                ))}
+              </Menu>
+
+              <Typography
+                variant="h5"
+                noWrap
+                component="p"
+                sx={{
+                  mr: 2,
+                  ml: 3,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "red",
+                  textDecoration: "none",
+                }}
+              >
+                RF
+              </Typography>
+            </Box>
+            <p></p>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {pages.map((page) => (
                 <NavLink
                   key={page.title}
@@ -120,90 +166,55 @@ const MainHeader: React.FC = () => {
                   }}
                   to={page.path}
                 >
-                  <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page.title}</Typography>
-                  </MenuItem>
+                  <Button
+                    key={page.title}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "inherit", display: "flex" }}
+                  >
+                    {page.title}
+                  </Button>
                 </NavLink>
-              ))}
-            </Menu>
 
-            <Typography
-              variant="h5"
-              noWrap
-              component="p"
-              sx={{
-                mr: 2,
-                ml: 3,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "red",
-                textDecoration: "none",
-              }}
-            >
-              RF
-            </Typography>
-          </Box>
-          <p></p>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <NavLink
-                key={page.title}
-                style={({ isActive, isPending }) => {
-                  return {
-                    color: isActive ? "white" : "lightgrey",
-                    textDecoration: "none",
-                  };
+                //Nav link here
+              ))}
+            </Box>
+
+            <SearchInput />
+            <Box sx={{ flexGrow: 0, ml: 2 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar
+                    alt="Marcus Ferud"
+                    src="/static/images/avatar/2.jpg"
+                  />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
                 }}
-                to={page.path}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
               >
-                <Button
-                  key={page.title}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "inherit", display: "flex" }}
-                >
-                  {page.title}
-                </Button>
-              </NavLink>
-
-              //Nav link here
-            ))}
-          </Box>
-
-          <SearchInput />
-          <Box sx={{ flexGrow: 0, ml: 2 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Marcus Ferud" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </AppBar>
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      )}
     </ThemeProvider>
   );
 };
--- src/pages/auth/AuthView.tsx
+++ src/pages/auth/AuthView.tsx
@@ -1,5 +1,67 @@
-const AuthView: React.FC = () => {
-    return <h4>Auth View</h4>
-}
+import React from "react";
 
-export default AuthView
+import { Box, TextField, Button } from "@mui/material";
+import {} from "@mui/icons-material";
+
+import classes from "./AuthView.module.scss";
+import { Link } from "react-router-dom";
+
+const AuthView: React.FC = () => {
+  return (
+    <>
+      <Box className={classes.background}>
+        <Box className={classes.cont}>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": {
+                m: 1,
+                width: "50%",
+                background: "rgba(48, 48, 48, 0.87)",
+                borderRadius: "6px",
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "grey",
+                },
+                "&:hover fieldset": {
+                  borderColor: "gray",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "white",
+                },
+              },
+
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="email" type="email" label="Email" />
+            <TextField id="password" type="password" label="Password" />
+            <Button
+              sx={{
+                color: "white",
+                backgroundColor: "red !important",
+                zIndex: 1,
+                width: "50%",
+                mt: "5%",
+              }}
+            >
+              <h4>Zaloguj się</h4>
+            </Button>
+            <Box className={classes.loginSignup}>
+              <p>Nie nasz jeszcze konta ? &nbsp; <Link to="/auth">Zarejestruj się</Link></p>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default AuthView;
