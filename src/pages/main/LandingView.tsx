3cf8ad52d25fe995b38eb7786eabda513bef6842
mariusz <mariusz.waldoch98@gmail.com>
2022-10-26 21:57:20 +0200

    landingPage banner

--- src/pages/main/LandingView.tsx
+++ src/pages/main/LandingView.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Box, Button } from "@mui/material";
+
+import classes from "./LandingView.module.scss";
 
 const LandingView: React.FC = () => {
-    return <h2>Landing page</h2>
-}
+  const navigate = useNavigate();
+  return (
+    <Box className={classes.banner}>
+      <Box className={classes.backdrop}>
+        <Box className={classes.header}>
+          <h1>Reactflix</h1>
+          <Button onClick={() => navigate('/auth')}>
+            <strong>Zaloguj się</strong>
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default LandingView;
