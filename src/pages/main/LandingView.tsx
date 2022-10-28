c14e183ab2f154e30c888e21ad54a967a1b1a0d5
mariusz <mariusz.waldoch98@gmail.com>
2022-10-28 21:27:52 +0200

    landPage email input

--- src/pages/main/LandingView.tsx
+++ src/pages/main/LandingView.tsx
@@ -1,20 +1,77 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid, TextField } from "@mui/material";
 
 import classes from "./LandingView.module.scss";
 
 const LandingView: React.FC = () => {
   const navigate = useNavigate();
+
   return (
     <Box className={classes.banner}>
       <Box className={classes.backdrop}>
         <Box className={classes.header}>
           <h1>Reactflix</h1>
-          <Button onClick={() => navigate('/auth')}>
+          <Button onClick={() => navigate("/auth")}>
             <strong>Zaloguj się</strong>
           </Button>
+        </Box>
+        <Box className={classes.info}>
+          <Grid
+            container
+            rowSpacing={1}
+            sx={{
+              marginInline: "25vw",
+            }}
+          >
+            <Grid item xs={12}>
+              <h1>
+                Nieograniczona oferta filmów, seriali, programów i nie tylko.
+              </h1>
+            </Grid>
+            <Grid item xs={12}>
+              <h2>Oglądaj wszędzie. Anuluj w każdej chwili.</h2>
+            </Grid>
+            <Grid item xs={12}>
+              <h3>
+                Zaczynamy oglądać? Wprowadź adres e-mail, aby utworzyć lub
+                odnowić konto.
+              </h3>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="email"
+                type="email"
+                label="Adres e-mail"
+                variant="filled"
+                sx={{
+                  width: "100%",
+                  backgroundColor: "white",
+                  borderRadius: "0",
+                }}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={4}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                sx={{
+                  color: "white",
+                  backgroundColor: "red",
+
+                }}
+              >
+                Rozpocznij
+              </Button>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
     </Box>
